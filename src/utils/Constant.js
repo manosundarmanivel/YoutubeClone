9668c3b9b72e859991d0e7d33453430308fc53de
manosundarmanivel <manosundarmanivel@gmail.com>
2023-07-26 19:47:11 +0530

    if applied this commit will use api key from .env file

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,4 +1,7 @@
-const API_KEY = "AIzaSyDiAFlAguIBSTcaNKKgjpSNHpdIeNBQbOw";
+
+const API_KEY  = process.env.REACT_APP_API_KEY;
+
+
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
