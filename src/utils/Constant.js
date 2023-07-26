aacbb23efeb016c4ecc0b52ceecccd0e655777c5
manosundarmanivel <manosundarmanivel@gmail.com>
2023-07-26 21:00:14 +0530

    if applied this commit will deploy the application

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -6,7 +6,7 @@ export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const YOUTUBE_LIVE_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=25&q=news&type=video&key=" +
   API_KEY;
